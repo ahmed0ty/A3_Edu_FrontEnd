5c5f3476bbe3977b163e0c7bbcfc9b0c6367795a
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-04-01 06:58:36 +0200

    third commit

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://a3-edu.onrender.com", {
   withCredentials: true,
 });
 export default socket;
